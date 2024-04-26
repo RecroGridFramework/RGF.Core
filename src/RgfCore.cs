1b09bd34cdbb6a93b0b95c9a8b378a4deabdf6fa
Tamás Mikola <73854810+TamasMikola@users.noreply.github.com>
2024-04-26 11:29:33 +0200

    8.2.0 (#8)
    * 8.2.0
    
    * RgfCore<TEntity>

---
+++ src/RgfCore.cs
@@ -0,0 +1,7 @@
+﻿
+namespace Recrovit.RecroGridFramework.Core;
+
+public partial class RgfCore<TEntity> : RecroGridDBC<TEntity> where TEntity : class
+{
+    public RgfCore(IRecroGridContext rgContext) : base(rgContext) { }
+}
